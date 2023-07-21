a2a44cfbd69e3147a612e91c94cfedae752db25d
Fernando Piacezzi <fernandopiacezzi@gmail.com>
2023-07-20 21:53:49 -0300

    fist commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto, VT323 } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const vt323 = VT323({ subsets: ['latin'], variable: '--font-VT323', weight: ['400'] })
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto', weight: ['400', '700'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${vt323.variable} ${roboto.variable} font-roboto`}>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,102 @@
-import Image from 'next/image'
+"use client"
+
+import { useRef } from "react"
+
+const mockData = [
+  { title: 'Product Project', image: 'https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80' },
+  { title: 'Engenering', image: "https://images.unsplash.com/photo-1643199319409-84b7066f45b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1752&q=80" },
+  { title: 'Coding', image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1772&q=80" },
+  { title: 'Robotics', image: "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" },
+  { title: 'Network', image: "https://plus.unsplash.com/premium_photo-1682145181120-73cfdfc8a36d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" },
+  { title: 'Hacking', image: "https://images.unsplash.com/photo-1624969862644-791f3dc98927?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" },
+]
 
 export default function Home() {
+  const carouselRef = useRef()
+
+  function mouseMove(e) {
+    if (carouselRef.current.dataset.mouseDownAt == '0') return;
+
+    const mouseDelta = parseFloat(carouselRef.current.dataset.mouseDownAt) - e.clientX,
+      maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100;
+
+    let nextPercentage = parseFloat(carouselRef.current.dataset.prevPercentage) + percentage;
+
+    nextPercentage = Math.min(nextPercentage, 0)
+    nextPercentage = Math.max(nextPercentage, -75)
+
+
+
+    carouselRef.current.dataset.percentage = nextPercentage
+    carouselRef.current.animate({
+      transform: `translateX(${nextPercentage}%)`
+
+    }, { duration: 800, fill: 'forwards' })
+
+    for (const image of carouselRef.current.getElementsByClassName('image')) {
+      image.animate({
+        objectPosition: `${nextPercentage + 100}% 50%`
+      }, { duration: 800, fill: 'forwards' })
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-zinc-900 overflow-hidden " >
+      <main className="min-w-max min-h-screen overflow-hidden m-auto grid grid-rows-2 max-h-fit grid-cols-[100vw] place-content-stretch">
+        <div className="px-24">
+
+          <aside className=' flex justify-between py-10 m-auto  '>
+            <div className='font-sans flex items-end text-4xl text-blue-500'>
+              <span>
+                Search-er
+              </span>
+            </div>
+            <nav className='flex gap-10 items-end'>
+              <a className='hover:underline decoration-blue-500 decoration-2 duration-200' href="/">About</a>
+              <a className='hover:underline decoration-blue-500 decoration-2 duration-200' href="/">Pricing</a>
+              <a className='hover:underline decoration-blue-500 decoration-2 duration-200' href="/">Contact</a>
+            </nav>
+          </aside>
+          <div >
+            <h1 className='text-8xl'>Create and<br />Search</h1>
+            <p className='text-zinc-500 ml-2 mt-2'>Create, search and read articles, blogs and research about your categories.</p>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className='select-none	cursor-grab active:cursor-grabbing relative w-max	pl-24 mt-2'
+          id="image-track"
+          data-mouse-down-at="0"
+          data-prev-percentage="0"
+          ref={carouselRef}
+          onMouseUp={(e) => {
+            carouselRef.current.dataset.mouseDownAt = 0;
+            carouselRef.current.dataset.prevPercentage = carouselRef.current.dataset.percentage
+          }}
+          onMouseDown={(e) =>
+            carouselRef.current.dataset.mouseDownAt = e.clientX
+          }
+          onMouseMove={mouseMove}
+          onScroll={() => console.log("scrl")}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className='flex flex-col gap-2 duration-500 hover:underline decoration-blue-500 decoration-2 '>
+            <img className="rounded-xl duration-200 image add bg-zinc-300" src="https://cdn3.iconfinder.com/data/icons/zeir-minimalism-1/25/add_new_category_item-1024.png" alt="" draggable="false" />
+            <span className='text-zinc-400 text-center'>New Categorie</span>
+          </div>
+          {mockData.map(data =>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            <div key={data.title} className='flex flex-col gap-2 duration-500 hover:underline decoration-blue-500 decoration-2 '>
+              <img className="rounded-xl duration-200 image" src={data.image} alt="" draggable="false" />
+              <span className='text-zinc-400 text-center'>{data.title}</span>
+            </div>
+          )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </div>
+      </main>
+    </div>
   )
 }
